035a1cde41cf494999132606ec528a5e82b6c27c
charg2dang <charg2dang@kakao.com>
2022-03-23 23:55:46 +0900

    간단 예제 테스트
    컨트롤러에서 뷰로 데이터 넘기기

---
+++ MVCTraning/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MVCTraning.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty( RequestId );
+    }
+}
