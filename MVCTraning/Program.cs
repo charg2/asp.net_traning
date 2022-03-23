1137c824380aba5648a99bd31cfba7f4edb6e8a3
charg2dang <charg2dang@kakao.com>
2022-03-24 01:05:36 +0900

    데이터 세팅용 SeedData 클래스 추가

--- MVCTraning/Program.cs
+++ MVCTraning/Program.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using MVCTraning.Data;
+using MVCTraning.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<MvcMovieContext>( options =>
@@ -12,6 +14,13 @@ builder.Services.AddDbContext<MvcMovieContext>( options =>
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
+
+using ( var scope = app.Services.CreateScope() )
+{
+    var services = scope.ServiceProvider;
+
+    SeedData.Initialize( services );
+}
 
 // Configure the HTTP request pipeline.
 if ( !app.Environment.IsDevelopment() )
